743c7b2b219a0ced124464ab73eb07b838d56e6c
vin123vin123 <vin33666@gmail.com>
2026-08-29 20:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
+    return pymongo.MongoClient(st.secrets["mongo"])
 
 client = init_connection()
 db = client.blogdb
